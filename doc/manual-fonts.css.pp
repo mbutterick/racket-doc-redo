5f271bfff3900f794ffae1eb1f77239fe8402f27
Matthew Butterick <mb@mbtype.com>
2017-11-19 19:46:13 -0800

    changes

--- doc/manual-fonts.css.pp
+++ doc/manual-fonts.css.pp
@@ -1,7 +1,7 @@
 #lang pollen
 ◊(require css-tools racket/string racket/file)
 
-◊(define do-base64 #f)
+◊(define do-base64 #t)
 ◊(define font-dir "manual-fonts/")
 
 ◊(define (embed-license which)
@@ -17,31 +17,31 @@
 
 
 ◊; Cooper-Hewitt
-◊(font-face-declaration "Cooper-Hewitt" (string-append font-dir "cooper-hewitt/webfonts-otf/cooperhewitt-book.woff") #:base64 do-base64)
+◊(font-face-declaration "Cooper-Hewitt" (string-append font-dir "cooper-hewitt/woff/cooperhewitt-book.woff") #:base64 do-base64)
 
 ◊(embed-license "Cooper-Hewitt")
 
 ◊; Charter
-◊(font-face-declaration "Charter" (string-append font-dir "charter/webfonts-otf/charter_regular.woff") #:base64 do-base64)
-◊(font-face-declaration "Charter" (string-append font-dir "charter/webfonts-otf/charter_italic.woff") #:font-style "italic"  #:base64 do-base64)
-◊(font-face-declaration "Charter" (string-append font-dir "charter/webfonts-otf/charter_bold.woff") #:font-weight "bold" #:base64 do-base64)
-◊;(font-face-declaration "Charter" (string-append font-dir "charter/webfonts-otf/charter_bold_italic.woff") #:font-weight "bold" #:font-style "italic" #:base64 do-base64)
+◊(font-face-declaration "Charter" (string-append font-dir "charter/woff/charter_regular.woff") #:base64 do-base64)
+◊(font-face-declaration "Charter" (string-append font-dir "charter/woff/charter_italic.woff") #:font-style "italic"  #:base64 do-base64)
+◊(font-face-declaration "Charter" (string-append font-dir "charter/woff/charter_bold.woff") #:font-weight "bold" #:base64 do-base64)
+◊;(font-face-declaration "Charter" (string-append font-dir "charter/woff/charter_bold_italic.woff") #:font-weight "bold" #:font-style "italic" #:base64 do-base64)
 
 ◊(embed-license "Charter")
 
 
 ◊; Fira
-◊(define fira-names '("Light" "Regular" "Medium" "SemiBold"))
+◊(define fira-names '("light" "regular" "medium" "bold"))
 ◊(define fira-weights '("300" "400" "500" "600"))
-◊(string-join (map (λ(name weight) (font-face-declaration "Fira" (string-append font-dir "fira/webfonts-ttf/FiraSans-" name ".woff") #:font-weight weight #:base64 do-base64)) fira-names fira-weights) "\n")
+◊(string-join (map (λ(name weight) (font-face-declaration "Fira" (string-append font-dir "fira/woff/firasans-" name ".woff") #:font-weight weight #:base64 do-base64)) fira-names fira-weights) "\n")
 
-◊(define fira-italic-names '("LightItalic" "Italic"))
+◊(define fira-italic-names '("LightItalic" "RegularItalic"))
 ◊(define fira-italic-weights '("300" "400"))
-◊(string-join (map (λ(name weight) (font-face-declaration "Fira" (string-append font-dir "fira/webfonts-ttf/FiraSans-" name ".woff") #:font-weight weight #:font-style "italic" #:base64 do-base64)) fira-italic-names fira-italic-weights) "\n")
+◊(string-join (map (λ(name weight) (font-face-declaration "Fira" (string-append font-dir "fira/woff/firasans-" name ".woff") #:font-weight weight #:font-style "italic" #:base64 do-base64)) fira-italic-names fira-italic-weights) "\n")
 
 
-◊(font-face-declaration "Fira-Mono" (string-append font-dir "fira/webfonts-ttf/FiraMono-Regular.woff") #:base64 do-base64)
-◊(font-face-declaration "Fira-Mono" (string-append font-dir "fira/webfonts-ttf/FiraMono-Medium.woff") #:font-weight 
+◊(font-face-declaration "Fira-Mono" (string-append font-dir "fira/woff/FiraMono-Regular.woff") #:base64 do-base64)
+◊(font-face-declaration "Fira-Mono" (string-append font-dir "fira/woff/FiraMono-Medium.woff") #:font-weight 
 "bold" #:base64 do-base64)
 
 ◊(embed-license "Fira")
