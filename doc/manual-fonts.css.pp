ef58e93d99022de01fbe90c342cb549f552b597c
Matthew Butterick <mb@mbtype.com>
2017-11-20 11:41:09 -0800

    tweaks

--- doc/manual-fonts.css.pp
+++ doc/manual-fonts.css.pp
@@ -17,22 +17,24 @@
 
 
 ◊; Cooper-Hewitt
-◊(font-face-declaration "Cooper-Hewitt" (string-append font-dir "cooper-hewitt/woff/cooperhewitt-book.woff") #:base64 do-base64)
+◊; tag as bold because h2 user agent style insists upon it
+◊(font-face-declaration "Cooper-Hewitt" (string-append font-dir "cooper-hewitt/woff/cooperhewitt-book.woff") #:font-weight "bold" #:base64 do-base64)
 
 ◊(embed-license "Cooper-Hewitt")
 
 ◊; Charter
-◊(font-face-declaration "Charter" (string-append font-dir "charter/woff/charter_regular.woff") #:base64 do-base64)
-◊(font-face-declaration "Charter" (string-append font-dir "charter/woff/charter_italic.woff") #:font-style "italic"  #:base64 do-base64)
-◊(font-face-declaration "Charter" (string-append font-dir "charter/woff/charter_bold.woff") #:font-weight "bold" #:base64 do-base64)
-◊;(font-face-declaration "Charter" (string-append font-dir "charter/woff/charter_bold_italic.woff") #:font-weight "bold" #:font-style "italic" #:base64 do-base64)
+◊; named Charter-Racket to keep it from colliding with iOS/Mac OS Charter
+◊(define charter-name "Charter-Racket")
+◊(font-face-declaration charter-name (string-append font-dir "charter/woff/charter_regular.woff") #:base64 do-base64)
+◊(font-face-declaration charter-name (string-append font-dir "charter/woff/charter_italic.woff") #:font-style "italic"  #:base64 do-base64)
+◊(font-face-declaration charter-name (string-append font-dir "charter/woff/charter_bold.woff") #:font-weight "bold" #:base64 do-base64)
 
 ◊(embed-license "Charter")
 
 
 ◊; Fira
 ◊(define fira-names '("light" "regular" "medium"))
-◊(define fira-weights '("300" "400" "500"))
+◊(define fira-weights '("300" "400" "600"))
 ◊(string-join (map (λ(name weight) (font-face-declaration "Fira" (string-append font-dir "fira/woff/firasans-" name ".woff") #:font-weight weight #:base64 do-base64)) fira-names fira-weights) "\n")
 
 ◊(define fira-italic-names '("LightItalic" "RegularItalic"))
@@ -42,6 +44,6 @@
 
 ◊(font-face-declaration "Fira-Mono" (string-append font-dir "fira/woff/FiraMono-Regular.woff") #:base64 do-base64)
 ◊(font-face-declaration "Fira-Mono" (string-append font-dir "fira/woff/FiraMono-Medium.woff") #:font-weight 
-"bold" #:base64 do-base64)
+"500" #:base64 do-base64)
 
 ◊(embed-license "Fira")
